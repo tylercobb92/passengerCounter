6e7ba04d7118ef5d17bddb797c2812e1f0b7ad5b
Tyler <tylercobb92@gmail.com>
2021-06-29 15:24:21 -0400

    add increment button to html, add js practice and psuedo code to app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+//document.getElementById("count-el").innerText = 5
+
+//let count = 5
+
+//console.log(count)
+
+//initialize count as 0
+//listen for clicks on increment button
+//add to count when button is clicked
+//change count-el in html 
+
+let count = 0
+function increment() {
+    console.log("BUTTON CLICKED")
+}
+
