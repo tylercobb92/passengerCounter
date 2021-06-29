51650348b4810610e6ae2e549df9712d3b36b54a
Tyler <tylercobb92@gmail.com>
2021-06-29 16:33:48 -0400

    countdown function added to js

---
+++ racecar.js
@@ -0,0 +1,28 @@
+//Using functions to write less code
+function countdown() {
+    console.log(5)
+    console.log(4)
+    console.log(3)
+    console.log(2)
+    console.log(1)
+}
+//setting up the race
+countdown()
+// console.log(5)
+// console.log(4)
+// console.log(3)
+// console.log(2)
+// console.log(1)
+
+//GO!
+//Players running race
+//race is finished
+
+//start new race
+countdown()
+// console.log(5)
+// console.log(4)
+// console.log(3)
+// console.log(2)
+// console.log(1)
+
