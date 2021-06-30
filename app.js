5e335ec81e9c1d4b1bfa466cb8787b5b69ea2b43
Tyler <tylercobb92@gmail.com>
2021-06-30 16:58:14 -0400

    cleaned up practice code in app.js

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-//document.getElementById("count-el").innerText = 5
-
-//let count = 5
-
-//console.log(count)
-
-//initialize count as 0
-//listen for clicks on increment button
-//add to count when button is clicked
-//change count-el in html 
-
 let count = 0
 let countEl = document.getElementById("count-el")
 
@@ -18,9 +7,3 @@ function increment() {
     countEl.innerText = count;
     // console.log(count)
 }
-
-//Random practice below here
-// function fourtyTwo() {
-//     console.log(42);
-// }
-// fourtyTwo()
