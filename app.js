3b950cda87c4690a60a37416d6d69e69d1f1f38b
Tyler <tylercobb92@gmail.com>
2021-07-01 14:24:33 -0400

    added paragraph tag to html for practice with strings in app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let count = 0
 let countEl = document.getElementById("count-el")
 
+
+
 function increment() {
     // console.log("BUTTON CLICKED");
     count += 1;
@@ -12,3 +14,27 @@ function increment() {
 function save() {
     console.log(count);
 }
+
+//random practice below
+
+// let username = "per"
+// let message = "You have three new notifications"
+// // console.log(message + ", " + username)
+// let messageToUser = (message + ", " + username + "!")
+// console.log(messageToUser)
+
+// let name = "Tyler"
+// let greeting = "Hi, my name is "
+// let myGreeting = greeting + name
+// console.log(myGreeting)
+
+// let points = "4"
+// let bonusPoints = "10"
+// let totalPoints = points + bonusPoints //410, string always wins
+
+let name = "Tyler Cobb"
+let greeting = "Welcome back, "
+let welcomeEl = document.getElementById("welcome-el")
+welcomeEl.innerText = greeting + name
+
+welcomeEl.innerText += "👋"
